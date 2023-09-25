dad8de5a88063beec43dbcafec86e6541380ee39
yay19599 <yay19599@gmail.com>
2023-09-25 14:18:45 +0800

    excel读取关键字数据

--- data/excel_handle.py
+++ data/excel_handle.py
@@ -3,11 +3,9 @@ import openpyxl
 
 class ReadExcel:
 
-
     def __init__(self, filename):
         self.workbook = openpyxl.load_workbook(filename)
         self.sheets = self.workbook.get_sheet_names()
-
 
     def read_rows(self, sheet):
         """
@@ -19,9 +17,21 @@ class ReadExcel:
         rows = ws.max_row
         cols = ws.max_column
         result = []
-        for i in range(1, rows + 1):
+        for i in range(2, rows + 1):  # 忽略第一行
             row_list = []
             for j in range(1, cols + 1):
                 row_list.append(ws.cell(i, j).value)
             result.append(row_list)
         return result
+
+    def get_keywords(self, keyword):
+        """
+        遍历所有sheet获取关键字的数据
+
+        return: 如果存在则返回, [关键字名称, 关键字的多个数据]
+        """
+        for sheet in self.sheets:
+            excel_data = self.read_rows(sheet)
+            for data in excel_data:
+                if data[0] == keyword:
+                    return data
