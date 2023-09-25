bf77ac5e9388323f1a4002e19e4bd1e4cee7fd31
yay19599 <yay19599@gmail.com>
2023-09-25 14:20:05 +0800

    字典断言新增忽略key选项

--- common/extra.py
+++ common/extra.py
@@ -22,16 +22,21 @@ def md5_string(in_str):
     return md5.hexdigest()
 
 
-def assert_dict_equal(old: dict, new: dict, flag=False):
+def assert_dict_equal(old: dict, new: dict, flag=False, ignore_keys=None):
+    if ignore_keys is None:
+        ignore_keys = []
     for key in old:
+        if key in ignore_keys:
+            continue
         if key in new.keys():
             if new[key] == old[key]:
                 flag = True
+                ignore_keys.append(key)
                 return flag
         elif isinstance(old[key], dict):
-            flag = assert_dict_equal(old[key], new, flag)
+            flag = assert_dict_equal(old[key], new, flag, ignore_keys)
         elif isinstance(old[key], list) and len(old[key]) > 1:
             if isinstance(old[key][0], dict):
                 for dic in old[key]:
-                    flag = assert_dict_equal(dic, new, flag)
+                    flag = assert_dict_equal(dic, new, flag, ignore_keys)
     return flag
