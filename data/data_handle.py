480ecd15843d18c26ae54b9dcf955552c0661c2b
yay19599 <yay19599@gmail.com>
2023-09-28 15:03:25 +0800

    增加步骤的耗时日志

--- data/data_handle.py
+++ data/data_handle.py
@@ -1,4 +1,6 @@
+import time
 from common.api import api
+from common.base import log
 from data.template import DataRender
 from keywords import KeyWordOperator
 
@@ -7,10 +9,13 @@ class DataHandle:
 
     def __init__(self):
         self.render_data = {}
+        self.time_consume = []
 
     def assemble_case(self, all_case):
         for case in all_case:
             if case[0] is not None:
+                start = time.time()
+                log.info(f"当前正在执行的步骤: {case[0]}")
                 if case[2]:
                     case[2] = DataRender.render(case[2], self.render_data)
                 case_name = case[1].split('=')
@@ -29,3 +34,6 @@ class DataHandle:
                     resp = api.execute(case_name, *case[2:5])
                     if tmp:
                         self.render_data[tmp] = resp
+                total_time = time.time() - start
+                log.info(f"步骤: {case[0]}, 耗时:{total_time}")
+                self.time_consume.append((case[0], total_time))
